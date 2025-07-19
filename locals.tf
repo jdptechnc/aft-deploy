433212267570a8caf571e28809dbdd09656d174e
Jared Priddy <jdptechnc@gmail.com>
2025-07-19 14:48:39 -0400

    Add files via upload

---
+++ locals.tf
@@ -0,0 +1,64 @@
+locals {
+  #########################################
+  # Control Tower Core Account Parameters
+  #########################################
+  ct_management_account_id = "480875379890"
+  log_archive_account_id   = "500746601129"
+  audit_account_id         = "104363823828"
+
+  #########################################
+  # General AFT Vars
+  #########################################
+  aft_management_account_id                             = "513282776145"
+  ct_home_region                                        = "us-east-1"
+  cloudwatch_log_group_retention                        = 3
+  backup_recovery_point_retention                       = 3 # Dynamo DB table recovery point retention in days
+  log_archive_bucket_object_expiration_days             = 3
+  aft_backend_bucket_access_logs_object_expiration_days = 3
+  aft_vpc_endpoints                                     = true
+  # maximum_concurrent_customizations = 5
+  # concurrent_account_factory_actions = 5
+  # global_codebuild_timeout = 60
+  # tags = {                          # optional tags to apply to all AFT resources
+  #   "Environment" = "Production"
+  # }
+
+  #########################################
+  # AFT Feature Flags - settings for accounts provisioned by AFT
+  #########################################
+  # aft_feature_cloudtrail_data_events      = false
+  # aft_feature_enterprise_support          = false
+  aft_feature_delete_default_vpcs_enabled = true
+
+  #########################################
+  # AFT Customer VCS Variables - only the variables required by github are shown here
+  # for other VCS providers, see the official AFT documentation
+  #########################################
+  aft_feature_branch_name                       = "main"
+  vcs_provider                                  = "github"
+  account_request_repo_name                     = "jdptechnc/aft-account-customizations"
+  global_customizations_repo_name               = "jdptechnc/aft-account-provisioning-customizations"
+  account_customizations_repo_name              = "jdptechnc/aft-account-request"
+  account_provisioning_customizations_repo_name = "jdptechnc/aft-global-customizations"
+  #########################################
+  # AFT Terraform Distribution Variables - only the variables required by community edition terraform are shown here
+  #########################################
+  # terraform_version = ""
+  terraform_distribution      = "oss"
+  # tf_backend_secondary_region = "us-west-2"   # uncomment to use a secondary region for the Terraform backend for AFT
+
+  #########################################
+  # AFT VPC Variables - only variables related to using existing vpc are shown here
+  #########################################
+  aft_enable_vpc      = true                    # Use a VPC instead of public internet
+  aft_customer_vpc_id = "vpc-0674aa445e44cf89a" # Use an existing VPC previously created for use by AFT management
+  aft_customer_private_subnets = [
+    "subnet-07c32b3b4ce9594d1"
+  ]
+
+  #########################################
+  # AFT Metrics Reporting Variables
+  #########################################
+  # aft_metrics_reporting = false
+
+}
---
+++ main.tf
@@ -0,0 +1,58 @@
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+terraform {
+  backend "s3" {
+    bucket       = "jdp-aft-module-backend-v2"
+    key          = "module-deployment/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true #S3 native locking
+    profile      = "jdptechnc-master-administrator"
+  }
+}
+
+module "aft" {
+  source = "github.com/aws-ia/terraform-aws-control_tower_account_factory"
+  # Required Vars
+  ct_management_account_id  = local.ct_management_account_id
+  log_archive_account_id    = local.log_archive_account_id
+  audit_account_id          = local.audit_account_id
+  aft_management_account_id = local.aft_management_account_id
+  ct_home_region            = local.ct_home_region
+
+  # General AFT vars
+  cloudwatch_log_group_retention                        = local.cloudwatch_log_group_retention
+  backup_recovery_point_retention                       = local.backup_recovery_point_retention
+  log_archive_bucket_object_expiration_days             = local.log_archive_bucket_object_expiration_days
+  aft_backend_bucket_access_logs_object_expiration_days = local.aft_backend_bucket_access_logs_object_expiration_days
+  aft_vpc_endpoints                                     = local.aft_vpc_endpoints
+  maximum_concurrent_customizations                     = local.maximum_concurrent_customizations
+  concurrent_account_factory_actions                    = local.concurrent_account_factory_actions
+  global_codebuild_timeout                              = local.global_codebuild_timeout
+  tags                                                  = local.tags
+
+  # terraform vars
+  terraform_distribution      = local.terraform_distribution
+  tf_backend_secondary_region = local.tf_backend_secondary_region
+
+  # VPC definition
+  aft_enable_vpc               = local.aft_enable_vpc
+  aft_customer_vpc_id          = local.aft_customer_vpc_id
+  aft_customer_private_subnets = local.aft_customer_private_subnets
+
+  # VCS Vars
+  vcs_provider                                  = local.vcs_provider
+  aft_feature_branch_name                       = local.aft_feature_branch_name
+  account_request_repo_name                     = local.account_request_repo_name
+  global_customizations_repo_name               = local.global_customizations_repo_name
+  account_customizations_repo_name              = local.account_customizations_repo_name
+  account_provisioning_customizations_repo_name = local.account_provisioning_customizations_repo_name
+
+  # AFT Feature Flags
+  aft_feature_cloudtrail_data_events      = local.aft_feature_cloudtrail_data_events
+  aft_feature_enterprise_support          = local.aft_feature_enterprise_support
+  aft_feature_delete_default_vpcs_enabled = local.aft_feature_delete_default_vpcs_enabled
+
+}
